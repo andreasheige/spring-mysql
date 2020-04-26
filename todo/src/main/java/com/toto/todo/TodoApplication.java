79107146e546459c8a3d510a081ff8cdcd6e19a0
Andreas Heige <a.heige.83@gmail.com>
2020-04-26 09:36:28 +0200

    updated app. file

--- todo/src/main/java/com/toto/todo/TodoApplication.java
+++ todo/src/main/java/com/toto/todo/TodoApplication.java
@@ -1,9 +1,11 @@
 package com.toto.todo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TodoApplication {
 
 	public static void main(String[] args) {
